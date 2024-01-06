5cd0d8b02f13ed0093dc127aa0e5055f5157f10a
Siddhant-Baranwal <siddhantbr10@gmail.com>
2024-01-06 15:33:14 +0530

    register

--- app.js
+++ app.js
@@ -13,17 +13,17 @@ config({
 })
 
 const router = express.Router();
-app.use(express.json());
-app.use(cookieParser());
-app.use(userRouter);
-app.use(taskRouter);
 app.use(
     cors({
-        origin: "*",
+        origin: [process.env.FRONTEND_URL],
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true,
     })
 );
+app.use(express.json());
+app.use(cookieParser());
+app.use(userRouter);
+app.use(taskRouter);
 
 app.get("/", (req, res)=>{
     res.send("Nice work.");
